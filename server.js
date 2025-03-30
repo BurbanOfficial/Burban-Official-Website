c9e1b44bdce21a3421663c9a4db713a74f1b6e5f
BurbanOfficial <burban.official@outlook.com>
2025-03-30 18:53:30 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4,21 +4,20 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const geoip = require('geoip-lite');
-
-app.get('/geoip', (req, res) => {
-  // Extraire la première adresse IP de la liste dans x-forwarded-for
-  const ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
-
-  const geo = geoip.lookup(ip); // Chercher l'information GeoIP
-  res.json({ ip, geo }); // Retourner l'IP et la localisation
-});
-
 // La clé Stripe doit être définie dans Render via une variable d'environnement (STRIPE_SECRET_KEY)
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Route de test pour GeoIP (facultative)
+app.get('/geoip', (req, res) => {
+  const xForwardedFor = req.headers['x-forwarded-for'];
+  const ip = xForwardedFor ? xForwardedFor.split(',')[0].trim() : req.connection.remoteAddress;
+  const geo = geoip.lookup(ip);
+  res.json({ ip, geo });
+});
 
 /**
  * Configuration des tarifs d'expédition par catégorie et par région.
@@ -46,7 +45,7 @@ const shippingRates = {
     australia: { unique: 9.79, additional: 1.80 },
     japan:     { unique: 5.99, additional: 2.00 },
     brazil:    { unique: 5.39, additional: 2.70 },
-    worldwide: { unique: 6.29, additional: 2.00 } // Valeur par défaut identique à Europe
+    worldwide: { unique: 6.29, additional: 2.00 }
   },
   category3: { // Troisième grille tarifaire (par défaut)
     us:        { unique: 9.79, additional: 4.90 },
@@ -94,7 +93,6 @@ const shippingRates = {
 
 /**
  * Détermine la catégorie d'un article en fonction de son nom.
- * La hiérarchie des conditions est la suivante :
  *  - Catégorie 1 : t-shirt, tshirt, débardeur, polo, crop-top
  *  - Catégorie 2 : sweat à capuche, pull, veste, pantalon de sport, pantalon de survêtement
  *  - Catégorie 4 : hoodie, sweatshirt, jacket, pants, joggers
@@ -126,7 +124,7 @@ function getCategory(item) {
 }
 
 /**
- * Calcule le coût de livraison pour un article, en fonction de sa catégorie, sa quantité et la région.
+ * Calcule le coût de livraison pour un article, en fonction de sa catégorie, de sa quantité et de la région.
  * @param {Object} item - L'article du panier (doit contenir name et quantity)
  * @param {string} region - La région de livraison (en minuscule)
  * @returns {number} Le coût en centimes.
@@ -166,11 +164,13 @@ app.post('/create-checkout-session', async (req, res) => {
     }
     
     // Détermination de la région de l'utilisateur via GeoIP
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    const ip = xForwardedFor ? xForwardedFor.split(',')[0].trim() : req.connection.remoteAddress;
     const geo = geoip.lookup(ip);
     let region = 'worldwide'; // Par défaut
     if (geo && geo.country) {
       const countryCode = geo.country.toLowerCase();
+      console.log("Détection GeoIP :", countryCode);
       if (countryToRegion[countryCode]) {
         region = countryToRegion[countryCode];
       } else if (euCountries.includes(countryCode)) {
@@ -212,7 +212,9 @@ app.post('/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       shipping_address_collection: {
-        allowed_countries: ['US','CA','AC','AD','AE','AF','AG','AI','AL','AM','AO','AQ','AR','AT','AU','AW','AX','AZ','BA','BB','BD','BE','BF','BG','BH','BI','BJ','BL','BM','BN','BO','BQ','BR','BS','BT','BV','BW','BY','BZ','CD','CF','CG','CH','CI','CK','CL','CM','CN','CO','CR','CV','CW','CY','CZ','DE','DJ','DK','DM','DO','DZ','EC','EE','EG','EH','ER','ES','ET','FI','FJ','FK','FO','FR','GA','GB','GD','GE','GF','GG','GH','GI','GL','GM','GN','GP','GQ','GR','GS','GT','GU','GW','GY','HK','HN','HR','HT','HU','ID','IE','IL','IM','IN','IO','IQ','IS','IT','JE','JM','JO','JP','KE','KG','KH','KI','KM','KN','KR','KW','KY','KZ','LA','LB','LC','LI','LK','LR','LS','LT','LU','LV','LY','MA','MC','MD','ME','MF','MG','MK','ML','MM','MN','MO','MQ','MR','MS','MT','MU','MV','MW','MX','MY','MZ','NA','NC','NE','NG','NI','NL','NO','NP','NR','NU','NZ','OM','PA','PE','PF','PG','PH','PK','PL','PM','PN','PR','PS','PT','PY','QA','RE','RO','RS','RU','RW','SA','SB','SC','SD','SE','SG','SH','SI','SJ','SK','SL','SM','SN','SO','SR','SS','ST','SV','SX','SZ','TA','TC','TD','TF','TG','TH','TJ','TK','TL','TM','TN','TO','TR','TT','TV','TW','TZ','UA','UG','UY','UZ','VA','VC','VE','VG','VN','VU','WF','WS','XK','YE','YT','ZA','ZM','ZW','ZZ'],
+        allowed_countries: [ 
+          'US','CA','AC','AD','AE','AF','AG','AI','AL','AM','AO','AQ','AR','AT','AU','AW','AX','AZ','BA','BB','BD','BE','BF','BG','BH','BI','BJ','BL','BM','BN','BO','BQ','BR','BS','BT','BV','BW','BY','BZ','CD','CF','CG','CH','CI','CK','CL','CM','CN','CO','CR','CV','CW','CY','CZ','DE','DJ','DK','DM','DO','DZ','EC','EE','EG','EH','ER','ES','ET','FI','FJ','FK','FO','FR','GA','GB','GD','GE','GF','GG','GH','GI','GL','GM','GN','GP','GQ','GR','GS','GT','GU','GW','GY','HK','HN','HR','HT','HU','ID','IE','IL','IM','IN','IO','IQ','IS','IT','JE','JM','JO','JP','KE','KG','KH','KI','KM','KN','KR','KW','KY','KZ','LA','LB','LC','LI','LK','LR','LS','LT','LU','LV','LY','MA','MC','MD','ME','MF','MG','MK','ML','MM','MN','MO','MQ','MR','MS','MT','MU','MV','MW','MX','MY','MZ','NA','NC','NE','NG','NI','NL','NO','NP','NR','NU','NZ','OM','PA','PE','PF','PG','PH','PK','PL','PM','PN','PR','PS','PT','PY','QA','RE','RO','RS','RU','RW','SA','SB','SC','SD','SE','SG','SH','SI','SJ','SK','SL','SM','SN','SO','SR','SS','ST','SV','SX','SZ','TA','TC','TD','TF','TG','TH','TJ','TK','TL','TM','TN','TO','TR','TT','TV','TW','TZ','UA','UG','UY','UZ','VA','VC','VE','VG','VN','VU','WF','WS','XK','YE','YT','ZA','ZM','ZW','ZZ'
+        ],
       },
       line_items: lineItems,
       discounts: [],
